69a5af817ed32221b6c6d6dc570ab0601ce6c915
atienoobwanda <atienoobwanda@gmail.com>
2022-07-03 07:43:26 +0300

    adding te login, sign up and setting up urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 
 from . import views
+from .views import (CustomerSignupView, HotelAdminSignupView,
+ CustomAuthToken, LogoutView, AdminOnlyView, CustomerOnlyView)
 
 urlpatterns = [
    
     path('', views.home),
+    path('signup/customer/', CustomerSignupView.as_view()),
+    path('signup/admin/', HotelAdminSignupView.as_view()),
+    path('login/',CustomAuthToken.as_view(), name='auth-token'),
+    path('logout/', LogoutView.as_view(), name='logout-view'),
+    path('freelance/dashboard/', CustomerOnlyView.as_view(), name='customer-dashboard'),
+    path('client/dashboard/', AdminOnlyView.as_view(), name='admin-dashboard'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,72 @@
 from django.shortcuts import render
 
 
+from django.http import request
+from rest_framework import generics, permissions, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .serializers import CustomerSignupSerializer, HotelAdminSignupSerializer, UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.views import APIView
+from .permissions import IsCustomerUser, IsHotelAdminUser
+
+class CustomerSignupView(generics.GenericAPIView):
+    serializer_class=CustomerSignupSerializer
+    def post(self, request, *args, **kwargs):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.save()
+        return Response({
+            "user":UserSerializer(user, context=self.get_serializer_context()).data,
+            "token":Token.objects.get(user=user).key,
+            "message":"account created successfully"
+        })
+
+
+class HotelAdminSignupView(generics.GenericAPIView):
+    serializer_class=HotelAdminSignupSerializer
+    def post(self, request, *args, **kwargs):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user=serializer.save()
+        return Response({
+            "user":UserSerializer(user, context=self.get_serializer_context()).data,
+            "token":Token.objects.get(user=user).key,
+            "message":"account created successfully"
+        })
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer=self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user=serializer.validated_data['user']
+        token, created=Token.objects.get_or_create(user=user)
+        return Response({
+            'token':token.key,
+            'user_id':user.pk,
+            'is_HotelAdmin':user.is_HotelAdmin
+        })
+
+
+class LogoutView(APIView):
+    def post(self, request, format=None):
+        request.auth.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+class AdminOnlyView(generics.RetrieveAPIView):
+    permission_classes=[permissions.IsAuthenticated&IsHotelAdminUser]
+    serializer_class=UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+class CustomerOnlyView(generics.RetrieveAPIView):
+    permission_classes=[permissions.IsAuthenticated&IsCustomerUser]
+    serializer_class=UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
 def home(request):
     return render(request, 'home.html')
