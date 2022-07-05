76baece9dfb12a887ef7f115a100b4c07dd35f75
atienoobwanda <atienoobwanda@gmail.com>
2022-07-05 12:48:05 +0300

    updating urls

--- api/urls.py
+++ api/urls.py
@@ -12,12 +12,14 @@ urlpatterns = [
     path('signup/admin/', HotelAdminSignupView.as_view()),
     path('login/',CustomAuthToken.as_view(), name='auth-token'),
     path('logout/', LogoutView.as_view(), name='logout-view'),
+
     path('customer/dashboard/', CustomerOnlyView.as_view(), name='customer-dashboard'),
+    
     path('admin/dashboard/', AdminOnlyView.as_view(), name='admin-dashboard'),
 
     path('hotels/', HotelList.as_view(), name='hotels'), # Configure api request
     path('hotel-detail/<int:pk>/', GetHotel.as_view(), name='hotel-detail'), # Configure api request
-    
+
     path('delete-hotel/<int:pk>/', DeleteHotel.as_view(), name=' deleteHotel'),
     path('update-hotel/<int:pk>/', UpdateHotel.as_view(), name='updateHotel'),
     path('new-hotel', AddHotel.as_view(), name='newHotel'),
