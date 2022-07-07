524f62a5bcc04077563feb37bdfa68061f68fb5a
atienoobwanda <atienoobwanda@gmail.com>
2022-07-07 13:32:40 +0300

    update

--- api/urls.py
+++ api/urls.py
@@ -16,8 +16,9 @@ urlpatterns = [
     path('login/',CustomAuthToken.as_view(), name='auth-token'),
     path('logout/', LogoutView.as_view(), name='logout-view'),
 
+    # path('customer/dashboard/<int:pk>/', CustomerOnlyView.as_view(), name='customer-dashboard'),
     path('customer/dashboard/', CustomerOnlyView.as_view(), name='customer-dashboard'),
-    
+
     path('admin/dashboard/', AdminOnlyView.as_view(), name='admin-dashboard'),
 
     path('hotel-detail/<int:pk>/', GetHotel.as_view(), name='hotel-detail'), # Configure api request
