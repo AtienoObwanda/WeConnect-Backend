652ac0837106e60bcfca89fbafb8f2849133c217
atienoobwanda <atienoobwanda@gmail.com>
2022-07-08 08:22:30 +0300

    updating serializer to fetch

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,10 @@ from rest_framework import serializers
 from api.models import User, Customer, owner, Facility, Hotel, Rooms, Booking
 
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['__all__']
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
