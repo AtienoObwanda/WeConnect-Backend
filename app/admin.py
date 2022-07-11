4714c79d845eeaf132609a45fb7643925180625e
atienoobwanda <atienoobwanda@gmail.com>
2022-07-11 03:23:39 +0300

    re-adding the models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+# admin.site.register(Customer)
+admin.site.register(Hotel)
+# admin.site.register(owner)
+admin.site.register(Room)
+admin.site.register(Booking)
+
--- app/models.py
+++ app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
+
+
+class Hotel (models.Model):
+    hotel_name = models.CharField(max_length=100)
+    description = models.TextField(blank= True)
+    facility1 = models.CharField(max_length=30, blank=True, null=True)
+    facility2 = models.CharField(max_length=30, blank=True, null=True)
+    facility3 = models.CharField(max_length=30, blank=True, null=True)
+    facility4 = models.CharField(max_length=30, blank=True, null=True)
+    facility5= models.CharField(max_length=30, blank=True, null=True)
+
+class Room (models.Model):
+    ROOM_TYPE = (
+        ('Stan', 'Standard'),
+        ('StanT', 'Standard Twin'),
+        ('Del', 'Deluxe'),
+        ('DelT', 'Deluxe Twin')
+        )
+    name = models.CharField(max_length=30, choices=ROOM_TYPE)
+    rate = models.PositiveIntegerField(blank=True, null=True)
+    image = models.ImageField(upload_to='images/')
+    hotel= models.ForeignKey(Hotel,  on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    
+    def create_room(self):
+        self.save
+
+class Booking(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    amount = models.ForeignKey(Room,  on_delete=models.CASCADE)
+    user =  models.ManyToManyField(User, related_name='bookings')
+    date = models.DateTimeField(auto_now_add=True)
+    phone = models.IntegerField(blank=True, null=True)
+    check_in= models.DateTimeField()
+    check_out = models.DateTimeField()
+    
+    def __str__(self):
+        return f'{self.date}'           
+    
