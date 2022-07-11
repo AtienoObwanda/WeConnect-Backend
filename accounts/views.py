836afc47e9c167f51949a4e7f727cde084c2332b
atienoobwanda <atienoobwanda@gmail.com>
2022-07-11 05:05:11 +0300

    updating urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def register(request):
+    
     return render(request, 'register.html')
+
+
 
 def login(request):
     return render(request, 'login.html')
