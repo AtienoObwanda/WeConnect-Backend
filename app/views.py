5470da9df60b3dffec4356193af17b558ac334b9
atienoobwanda <atienoobwanda@gmail.com>
2022-07-11 04:13:03 +0300

    adding a view for fetching hotels

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework.permissions import AllowAny
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from app.models import *
@@ -12,3 +15,13 @@ def hotels(request):
     return render(request, 'hotels.html', {'hotels':hotels})
 
 
+class HotelDetailList(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes= [TemplateHTMLRenderer]
+    template_name='hotelPage.html'
+
+    def get(self, request, pk):
+        hotel = get_object_or_404(Hotel, pk=pk)
+        rooms = Room.objects.filter(hotel_id=pk)
+        
+        return Response({'hotel': hotel, 'rooms': rooms})
