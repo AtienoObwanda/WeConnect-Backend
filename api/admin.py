683dd31b3d87f5932430ac3558559224c106124f
atienoobwanda <atienoobwanda@gmail.com>
2022-07-14 13:31:19 +0300

    hh

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Customer)
-admin.site.register(Hotel)
-admin.site.register(owner)
-admin.site.register(Room)
-admin.site.register(Booking)
+# admin.site.register(Customer)
+# admin.site.register(Hotel)
+# admin.site.register(owner)
+# admin.site.register(Room)
