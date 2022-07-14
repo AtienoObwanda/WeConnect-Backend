1062f35f8718d913827262204795864c51562b8b
atienoobwanda <atienoobwanda@gmail.com>
2022-07-14 11:13:45 +0300

    adding readme.  adding login

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # WeConnect-Backend
 We Connect is an online booking platform for hotels. As the name, we connect users who are in search of hotels  and hotels showcase their services!
+
+
+#API ENDPOINTS: 
+**** 
+
+1. Sign up as a user: https://weconnekt.herokuapp.com/api/signup/customer/
+**** 
+2. Login as a user:
+****
+
+3. Sign up as an admin: https://weconnekt.herokuapp.com/api/signup/admin/
+
+**** 
+4. Login as admin:
+**** 
+**** 
+**** 
+**** 
+**** 
+**** 
+**** 
--- api/views.py
+++ api/views.py
@@ -59,7 +59,7 @@ class HotelAdminSignupView(generics.GenericAPIView):
             "message":"account created successfully"
         })
 
-class CustomAuthToken(ObtainAuthToken): # Login for both admin and customer accounts
+class CustomAuthToken(ObtainAuthToken, generics.GenericAPIView): # Login for both admin and customer accounts
     def post(self, request, *args, **kwargs):
         serializer=self.serializer_class(data=request.data, context={'request':request})
         serializer.is_valid(raise_exception=True)
